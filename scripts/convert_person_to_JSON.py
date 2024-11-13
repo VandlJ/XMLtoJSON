c85bb53a7be2ec916fd041bdd7f31021556f569d
Jan Vandlíček <jan.vandlicek@gmail.com>
2024-11-13 14:48:24 +0100

    person to JSON runnable - automatic

--- scripts/convert_person_to_JSON.py
+++ scripts/convert_person_to_JSON.py
@@ -9,6 +9,9 @@ from convert.personConvert import parsePersonXML
 input_folder = "../test_data/Namen"
 output_folder = "../test_data/Namen_JSON"
 
+# Create the output folder if it does not exist
+os.makedirs(output_folder, exist_ok=True)
+
 # Iterate through the files in the input folder
 for file_name in os.listdir(input_folder):
     # Construct the full paths to the input and output files
@@ -18,7 +21,6 @@ for file_name in os.listdir(input_folder):
     modified_text = parsePersonXML(input_file_path).to_json()
     # Open the output file
 
-    #TO-DO Create folder
     with open(output_file_path, "w") as output_file:
         # Write the modified text to the output file
         output_file.write(modified_text)
